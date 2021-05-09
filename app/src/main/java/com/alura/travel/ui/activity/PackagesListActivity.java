e36fcd937bac96a1a746b450cf5a8caa896c3fc1
Pedro Henrique G Ramalho <phgr123@gmail.com>
2021-05-09 18:49:44 -0300

    Implementação - Conhecimentos de Constraints Layout

---
+++ app/src/main/java/com/alura/travel/ui/activity/PackagesListActivity.java
@@ -0,0 +1,16 @@
+package com.alura.travel.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alura.travel.R;
+
+public class PackagesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packages_list);
+    }
+}
